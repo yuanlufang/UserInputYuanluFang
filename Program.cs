ef22fcb6d0dd7f3de8b325f312aca8a71b55cfef
yuanlufang@mail.usf.edu <yuanlufang@mail.usf.edu>
2020-01-31 16:26:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserInputYuanluFang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Yuanlu Fang!");
+            Console.WriteLine("Please enter your message below and hit ENTER:");
+            string userTypeInMessage;
+            userTypeInMessage = Console.ReadLine();
+            Console.WriteLine("You input was:");
+            Console.WriteLine(userTypeInMessage);
+            
+        }
+    }
+}
